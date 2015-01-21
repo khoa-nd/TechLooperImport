2862ea9780689aec2927286b2201b27feb75a52d
Phuong H <phuonghqh@gmail.com>
2015-01-21 15:06:26 +0700

    Initialize empty project

---
+++ src/main/java/com/techlooper/imports/vnw/VietnamworksImportJob.java
@@ -0,0 +1,11 @@
+package com.techlooper.imports.vnw;
+
+/**
+ * Created by phuonghqh on 1/21/15.
+ */
+public class VietnamworksImportJob {
+
+  public static void main(String[] args) {
+    System.out.println(VietnamworksImportJob.class);
+  }
+}
