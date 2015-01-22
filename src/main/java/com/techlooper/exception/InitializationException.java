759f1cf80da0225f4fb85bf5e0e8fc62e5f24f02
Phuong H <phuonghqh@gmail.com>
2015-01-22 14:26:47 +0700

    Init version of GitHub import function

---
+++ src/main/java/com/techlooper/exception/InitializationException.java
@@ -0,0 +1,11 @@
+package com.techlooper.exception;
+
+/**
+ * Created by phuonghqh on 1/21/15.
+ */
+public class InitializationException extends Error {
+
+  public InitializationException(String error) {
+    super(error);
+  }
+}
