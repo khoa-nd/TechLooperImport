226953ab31208eea9b69d17b6a5109d5d492b2e8
hamedshayan@gmail.com <Hamed9243>
2015-01-23 18:36:30 +0700

    Adding some twist in order to fetch more and crawl better.

---
+++ src/test/java/com.techlooper.jobpostcrawlers/JobStreetCrawlerTest.java
@@ -0,0 +1,19 @@
+package com.techlooper.jobpostcrawlers;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by chris on 1/23/15.
+ */
+public class JobStreetCrawlerTest {
+
+    @Test
+    @Ignore("No Need to test for now")
+    public void testCrawl() throws Exception {
+        JobStreetCrawler crawler = new JobStreetCrawler();
+        for(int counter = 200; counter < 229; counter++) {
+            crawler.crawl(counter);
+        }
+    }
+}
