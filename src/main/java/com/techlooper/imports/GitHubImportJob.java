82185a3b944969fccd745978bbe270909d6c399b
Phuong H <phuonghqh@gmail.com>
2015-01-23 18:47:55 +0700

    Complete function

--- src/main/java/com/techlooper/imports/GitHubImportJob.java
+++ src/main/java/com/techlooper/imports/GitHubImportJob.java
@@ -1,122 +1,108 @@
 package com.techlooper.imports;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.importio.api.clientlite.ImportIO;
-import com.importio.api.clientlite.MessageCallback;
-import com.importio.api.clientlite.data.Query;
-import com.importio.api.clientlite.data.QueryMessage;
 import com.techlooper.utils.PropertyManager;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.*;
-import java.util.concurrent.CountDownLatch;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by phuonghqh on 1/22/15.
  */
 public class GitHubImportJob {
 
-  private static final Boolean[] hasNextPage = {Boolean.TRUE};
+  private static final Map<String, Boolean> hasNextPage = new HashMap<>();
 
-  private static Integer interval = 0;
+//  private static final CountDownLatch latch = new CountDownLatch(1000000);
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    if (args.length != 2) {
-      System.out.println("Usage example: mvn clean install -Dcountry=vietnam -DoutputDirectory=/techlooper/github/vietnam/");
-      return;
-    }
-
     String country = args[0];
-    String output = args[1].endsWith("/") ? args[1] : args[1] + "/";
 
-    UUID userId = UUID.fromString(PropertyManager.properties.getProperty("import.io.userId"));
-    ImportIO client = new ImportIO(userId, PropertyManager.properties.getProperty("import.io.apiKey"));
-    client.connect();
-
-    CountDownLatch latch = new CountDownLatch(1);
-
-    MessageCallback messageCallback = (query, message, progress) -> {
-      if (message.getType() == QueryMessage.MessageType.MESSAGE) {
-        HashMap<String, Object> resultMessage = (HashMap<String, Object>) message.getData();
-        String queryUrl = query.getInput().get("webpage/url").toString();
-        if (((List) resultMessage.get("results")).size() == 0 || resultMessage.containsKey("errorType")) {
-          System.out.println("Error => Stop query: " + queryUrl);
-          synchronized (hasNextPage) {
-            hasNextPage[0] = Boolean.FALSE;
-          }
-        }
-        else {
-          List<Object> results = (List<Object>) resultMessage.get("results");
-
-          System.out.println("Success => query: " + queryUrl);
-          System.out.println("Result size: " + results.size());
-          if (results.size() > 0) {
-            ObjectMapper mapper = new ObjectMapper();
-            try {
-//              synchronized (interval) {
-              String page = queryUrl.substring(queryUrl.indexOf("p=") + "p=".length(), queryUrl.indexOf("p=") + 3);
-              String period = queryUrl.substring(queryUrl.indexOf("created:") + "created:".length(), queryUrl.indexOf("created:") + 30);
-              BufferedWriter writer = Files.newBufferedWriter(Paths.get(output + period + "." + page + ".json"),
-                StandardCharsets.UTF_8, StandardOpenOption.CREATE);
-              mapper.writeValue(writer, results);
-              writer.close();
-
-//                System.out.println("Success saved => query: " + queryUrl + ", file-name-interval: " + interval);
-//              }
-            }
-            catch (IOException e) {
-              e.printStackTrace(System.err);
-            }
-          }
-        }
-      }
-
-      if (progress.isFinished()) {
-        System.out.println("Process is finished.");
-        latch.countDown();
-      }
-    };
-
-    Map<String, Object> queryInput = new HashMap<>();
-    List<UUID> connectorGuids = Arrays.asList(UUID.fromString(PropertyManager.properties.getProperty("import.io.connector.github")));
-
-    String urlTemplate = PropertyManager.properties.getProperty("github.user.searchTemplate");
     int currentYear = Calendar.getInstance(Locale.US).get(Calendar.YEAR);
     for (int year = 2007; year <= currentYear; ++year) {
-      String createdFrom = year + "-01-01";
-      String createdTo = year + "-06-30";
-      doQuery(country, client, messageCallback, queryInput, connectorGuids, urlTemplate, createdFrom, createdTo);
-      createdFrom = year + "-07-01";
-      createdTo = year + "-12-31";
-      doQuery(country, client, messageCallback, queryInput, connectorGuids, urlTemplate, createdFrom, createdTo);
+      doQuery(country, year + "-01-01", year + "-06-30");
+      doQuery(country, year + "-07-01", year + "-12-31");
     }
-
-//    latch.await();
-
-    client.disconnect();
   }
 
-  private static void doQuery(String country, ImportIO client, MessageCallback messageCallback,
-                              Map<String, Object> queryInput, List<UUID> connectorGuids, String urlTemplate,
-                              String createdFrom, String createdTo) throws IOException, InterruptedException {
-    synchronized (hasNextPage) {
-      hasNextPage[0] = Boolean.TRUE;
-    }
+  private static void doQuery(String country, String createdFrom, String createdTo) throws IOException, InterruptedException {
+    String userId = PropertyManager.properties.getProperty("import.io.userId");
+    String apiKey = PropertyManager.properties.getProperty("import.io.apiKey");
+    String urlTemplate = PropertyManager.properties.getProperty("github.user.searchTemplate");
+    String connectorId = PropertyManager.properties.getProperty("import.io.connector.github");
 
-    for (int i = 1; i < 101 && hasNextPage[0]; i++) {
-      String queryUrl = String.format(urlTemplate, i, country, createdFrom, createdTo);
+    String period = createdFrom + ".." + createdTo;
+    boolean hasNext = true;
+
+    for (int pageNumber = 1; pageNumber < 101 && hasNext; ) {
+      String queryUrl = String.format(urlTemplate, pageNumber, country, createdFrom, createdTo);
       System.out.println("Query using url: " + queryUrl);
-      queryInput.put("webpage/url", queryUrl);
-      Query query = new Query();
-      query.setConnectorGuids(connectorGuids);
-      query.setInput(queryInput);
-      client.query(query, messageCallback);
-      Thread.sleep(3000);
+
+      HttpClient httpClient = HttpClients.createDefault();
+      HttpPost p = new HttpPost(String.format("https://api.import.io/store/data/%s/_query?_user=%s&_apikey=%s",
+        connectorId, userId, URLEncoder.encode(apiKey, "UTF-8")));
+
+      String json = String.format("{ \"input\": {\"webpage/url\": \"%s\"} }", queryUrl);
+      p.setEntity(new StringEntity(json, ContentType.create("application/json")));
+
+      HttpResponse r = httpClient.execute(p);
+      BufferedReader rd = new BufferedReader(new InputStreamReader(r.getEntity().getContent(), "UTF-8"));
+      StringBuilder content = new StringBuilder();
+      String line = "";
+      while ((line = rd.readLine()) != null) {
+        content.append(line);
+      }
+      System.out.println("Response: " + content);
+      if (content.indexOf("I/O Error getting page.") > 0) {
+        System.out.println("I/O Error getting page => try it again.");
+        continue;
+      }
+      else {
+        ++pageNumber;
+      }
+
+      int resultIndex = content.indexOf("\"results\":[");
+      if (resultIndex < 0) {
+        System.out.println("No Result => query: " + queryUrl);
+        hasNext = false;
+        continue;
+      }
+      System.out.println("Success => query: " + queryUrl);
+
+      int endResultIndex = content.indexOf("],\"cookies\"", resultIndex);
+      json = content.substring(resultIndex + "\"results\":[".length() - 1, endResultIndex + 1);
+      String filename = country + "." + period + "." + (pageNumber - 1) + ".json";
+      if ("[]".equals(json)) {
+        hasNext = false;
+        System.out.println("Result is empty => not write to file: " + filename);
+        continue;
+      }
+
+      System.out.println("OK => wrote to file: " + filename);
+      try {
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get(filename), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+        writer.write(json);
+        writer.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace(System.err);
+      }
     }
   }
 }
