0532f3ce9b9fde713536bef2b47d892bd39f2c4d
Chris Shayan <chris@navigosgroup.com>
2015-03-01 23:04:43 +0700

    Now can search by username on slideshare then find the slides, perhaps we can use this to enrich the profile of user when he gave us his username on TechLooper.
    Besides, perhaps we can use this on VietnamWorks to help user application email or even RS.

--- src/test/java/com/techlooper/service/SlideShareServiceTest.java
+++ src/test/java/com/techlooper/service/SlideShareServiceTest.java
@@ -3,22 +3,32 @@ package com.techlooper.service;
 
 import com.benfante.jslideshare.messages.Slideshow;
 import com.benfante.jslideshare.messages.Tag;
+import com.benfante.jslideshare.messages.User;
 import com.techlooper.configuration.SlideShareConfiguration;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class SlideShareServiceTest {
 
+    ApplicationContext applicationContext;
+    SlideShareService service;
+
+    @Before
+    public void setApplicationContext() {
+        this.applicationContext = new AnnotationConfigApplicationContext(SlideShareConfiguration.class);
+        this.service =  (SlideShareService) applicationContext.getBean("slideShareService");
+    }
+
     @Test
     public void testSearchByTag() throws Exception {
-        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SlideShareConfiguration.class);
-        final SlideShareService service = (SlideShareService) applicationContext.getBean("slideShareService");
         final Tag java = service.searchByTag("java", 0, 10);
 
         assertNotNull(java);
@@ -30,5 +40,24 @@ public class SlideShareServiceTest {
             assertNotNull(slideshow.getTitle());
             System.out.println("slideshow.getTitle() = " + slideshow.getTitle());
         });
+    }
+
+    @Test
+    public void testSearchByUsername() throws Exception {
+        final User chrisshayan = service.searchByUsername("chrisshayan");
+        assertNotNull(chrisshayan);
+        assertEquals("chrisshayan", chrisshayan.getName());
+
+        final List<Slideshow> slideshows = chrisshayan.getSlideshows();
+        assertNotNull(slideshows);
+        slideshows.stream().forEach(slideshow -> {
+            assertNotNull(slideshow.getTitle());
+            System.out.println("slideshow.getTitle() = " + slideshow.getTitle());
+        });
+    }
+
+    @Test(expected = Exception.class)
+    public void testSearchByUsernameException() throws Exception {
+        service.searchByUsername("chrisshayanssssss");
     }
 }
