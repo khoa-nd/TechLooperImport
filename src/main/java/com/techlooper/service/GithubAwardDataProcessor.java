2de4e6f8c7f096e8b5e1918cc35abde58777261a
khoa-nd <ndkhoa.is@gmail.com>
2015-03-27 15:17:14 +0700

    Github Award : Implement Data Processor converts JSON raw from ImportIO to User Model In ES

---
+++ src/main/java/com/techlooper/service/GithubAwardDataProcessor.java
@@ -0,0 +1,76 @@
+package com.techlooper.service;
+
+import com.techlooper.entity.UserImportEntity;
+import com.techlooper.pojo.GithubAwardModel;
+import com.techlooper.pojo.GithubAwardResponse;
+import com.techlooper.pojo.SocialProvider;
+import com.techlooper.pojo.UserImportLocationRank;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by NguyenDangKhoa on 3/27/15.
+ */
+@Service
+public class GithubAwardDataProcessor {
+
+    public void process(UserImportEntity userImportEntity, GithubAwardResponse githubAwardResponse) {
+        Map<String, Object> profile = (Map<String, Object>) userImportEntity.getProfiles().get(SocialProvider.GITHUB);
+        if (profile != null) {
+            List<String> currentSkills = (List<String>) profile.get("skills");
+            Map<String, List<UserImportLocationRank>> ranks = (Map<String, List<UserImportLocationRank>>) profile.get("ranks");
+
+            List<GithubAwardModel> languageRanks = githubAwardResponse.getResults();
+            for (GithubAwardModel languageRank : languageRanks) {
+                processLanguageRank(languageRank);
+                List<UserImportLocationRank.LocationRank> locations = new ArrayList<>();
+
+                int[] rankTokens = processRankByLocation(languageRank.getCityRank());
+                locations.add(new UserImportLocationRank.LocationRank("city", languageRank.getCity(), rankTokens[0], rankTokens[1]));
+                rankTokens = processRankByLocation(languageRank.getCountryRank());
+                locations.add(new UserImportLocationRank.LocationRank("country", languageRank.getCountry(), rankTokens[0], rankTokens[1]));
+                rankTokens = processRankByLocation(languageRank.getWorldwideRank());
+                locations.add(new UserImportLocationRank.LocationRank("worldwide", "worldwide", rankTokens[0], rankTokens[1]));
+
+                String updatedTime = formatUpdatedDateTime(new Date());
+                UserImportLocationRank locationRank = new UserImportLocationRank(
+                        updatedTime, languageRank.getRepos(), languageRank.getStars(), locations);
+
+                List<UserImportLocationRank> locationRanks = ranks.get(languageRank.getLanguage());
+                if (locationRanks != null) {
+                    locationRanks.add(locationRank);
+                } else {
+                    locationRanks = new ArrayList<>();
+                    locationRanks.add(locationRank);
+                    ranks.put(languageRank.getLanguage(), locationRanks);
+                }
+            }
+        }
+    }
+
+    private String formatUpdatedDateTime(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+        return sdf.format(date);
+    }
+
+    private void processLanguageRank(GithubAwardModel languageRank) {
+        // ImportIO responds language with "ranking" word, remove it
+        languageRank.setLanguage(languageRank.getLanguage().replace(" ranking", "").toLowerCase());
+
+        languageRank.setCityRank(languageRank.getCityRank().replaceAll(" ", ""));
+        languageRank.setCountryRank(languageRank.getCountryRank().replaceAll(" ", ""));
+        languageRank.setWorldwideRank(languageRank.getWorldwideRank().replaceAll(" ", ""));
+    }
+
+
+    private int[] processRankByLocation(String locationRank) {
+        if (StringUtils.isNotEmpty(locationRank)) {
+            String[] tokens = locationRank.split("/");
+            return Arrays.stream(tokens).mapToInt(Integer::valueOf).toArray();
+        }
+        return new int[]{0, 0};
+    }
+}
--- src/test/java/com/techlooper/utils/ImportIOConnectionTest.java
+++ src/test/java/com/techlooper/utils/ImportIOConnectionTest.java
@@ -12,7 +12,7 @@ public class ImportIOConnectionTest {
         ImportIOExtractor importIOExtractorInfo = new ImportIOExtractor("5fc2f5f9-026a-46f2-bb91-00d84ab8a2fd",
                 "4c46b4b2-e818-4462-bf47-bf89165c3ace",
                 "CZs+sxsZFF/u3dBPO6Y+yYww1AWBRbIM20cWB5f0HXhcgGl36PmpjzRgwmcDA6eSUMkWmRQ9CiNIwGK2vfuWqg==");
-        String inputUrl = "http://github-awards.com/users/mbostock";
+        String inputUrl = "http://github-awards.com/users/chrisshayan";
         GithubAwardResponse response = ImportIOConnection.fetchContent(importIOExtractorInfo, inputUrl);
         assertNull(response.getError());
         assertTrue(response.getResults().size() > 0);
@@ -23,7 +23,7 @@ public class ImportIOConnectionTest {
         ImportIOExtractor importIOExtractorInfo = new ImportIOExtractor("5fc2f5f9-026a-46f2-bb91-00d84ab8a2fd",
                 "4c46b4b2-e818-4462-bf47-bf89165c3ace",
                 "WRONG API KEY");
-        String inputUrl = "http://github-awards.com/users/mbostock";
+        String inputUrl = "http://github-awards.com/users/chrisshayan";
         GithubAwardResponse response = ImportIOConnection.fetchContent(importIOExtractorInfo, inputUrl);
         assertNotNull(response.getError());
     }
